1f3392e0ab81b3b745559513935b9bfe9314ed78
cjosue13 <solomaj12@gmail.com>
2018-10-29 22:26:09 -0600

    nada (Ignorar)

--- ProyectoProgra3/src/proyectoprogra3/ProyectoProgra3.java
+++ ProyectoProgra3/src/proyectoprogra3/ProyectoProgra3.java
@@ -6,12 +6,7 @@
 package proyectoprogra3;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import proyectoprogra3.util.FlowController;
 
 /**
@@ -28,7 +23,6 @@ public class ProyectoProgra3 extends Application {
        // stage.initStyle(StageStyle.TRANSPARENT);
         
         FlowController.getInstance().InitializeFlow(stage,null);
-        System.out.println("ENTRO");
         FlowController.getInstance().goMain();
     }
 
